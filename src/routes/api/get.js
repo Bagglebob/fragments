86413b468b8fc8192890c065e80c01dda6904b22
Bagglebob <fawad-arshad96@hotmail.com>
2025-01-18 14:42:16 -0500

    Began defining api endpoints localhost:8080/v1/fragments

---
+++ src/routes/api/get.js
@@ -0,0 +1,13 @@
+// src/routes/api/get.js
+// localhost:8080/v1/fragments endpoint
+/**
+ * Get a list of fragments for the current user
+ */
+module.exports = (req, res) => {
+  // TODO: this is just a placeholder. To get something working, return an empty array...
+  res.status(200).json({
+    status: 'ok',
+    // TODO: change me
+    fragments: [],
+  });
+};
